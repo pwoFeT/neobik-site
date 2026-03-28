fc2f02c1e362f8e8058e5d1300ec8ac42763b906
Gilles ANSELME <gilles@pwofet-mac-mini.home>
2026-03-28 10:32:26 -0400

    fix: isolate Keystatic layout from site styles

--- app/keystatic/layout.tsx
+++ app/keystatic/layout.tsx
@@ -3,11 +3,5 @@ export default function KeystaticLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body style={{ margin: 0, padding: 0, background: 'white' }}>
-        {children}
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
